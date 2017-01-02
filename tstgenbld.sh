6cee5e0515e193b674db4fbb8fb38adec4301047
Jim Klimov <jim@jimklimov.com>
2017-01-02 12:43:45 +0100

    Problem: tstgenbld.sh scripting style is unconventional and has errors
    Solution: fix trailing `&&` to `&& \` etc.; always `exit $?` explicitly (not just on success) and comment why not drop it at all; double-quote echoed and argument strings; fix determination of `$finalresult`; mark the `initial cleanup` phase as such; use `mv -f` for log renaming; drop `test $? == 0` nonsense

--- tstgenbld.sh
+++ tstgenbld.sh
@@ -46,20 +46,21 @@
 #
 
 # set next line to "-o xtrace" if debugging
+# note that to simplify such tracing, some loops below explicitly "exit $?"
 XTRACE="+o xtrace"
 set ${XTRACE}
 
-export STARTDATE=`date`
+export STARTDATE="`date`"
 
 # directory where products will be installed
 BUILD_PREFIX=${PWD}/tmp
-echo Projects will be built to here "${BUILD_PREFIX}"
+echo "Projects will be built and installed to here: ${BUILD_PREFIX}"
 #read -p "Press ENTER to continue: "
 
 # directory where git projects will be cloned will be ${GITPROJECTS}
 GITROOT=${PWD}
 GITPROJECTS=${GITROOT}/gitprojects
-echo Projects will be cloned to here "${GITPROJECTS}"
+echo "Projects will be cloned to here: ${GITPROJECTS}"
 #read -p "Press ENTER to continue: "
 
 # user to get the zeromq projects on github
@@ -69,23 +70,23 @@ JEDISCT1=jedisct1
 
 function cleanRepositories() {
     find ${GITPROJECTS} | xargs rm -rf
-    test -d ${GITPROJECTS} &&
+    test -d ${GITPROJECTS} && \
             read -p "Error: Manually delete ${GITPROJECTS} then press ENTER: "
     test -d ${GITPROJECTS} && return 1 || return 0
 }
 
 # cleanup previous build, if any
 test -d ${BUILD_PREFIX} && find ${BUILD_PREFIX} | xargs rm -rf
-test -d ${BUILD_PREFIX} &&
+test -d ${BUILD_PREFIX} && \
             read -p "Error: Manually delete ${BUILD_PREFIX} then press ENTER: "
 mkdir ${BUILD_PREFIX}
 
 # only clean the repositories if using temporary repository area
-test "${1,,}" == "clean" && test -d ${GITPROJECTS} &&
+test "${1,,}" == "clean" && test -d ${GITPROJECTS} && \
 (
-    test "${PWD}" == "${GITROOT}" && cleanRepositories ||
+    test "${PWD}" == "${GITROOT}" && cleanRepositories || \
             read -p "Warning: Please do the clean manually."
-    test "${PWD}" != "${GITROOT}" && echo Clean done.
+    test "${PWD}" != "${GITROOT}" && echo "Clean done, exiting."
     exit 0
 ) && exit 0
 
@@ -94,9 +95,9 @@ test -d ${GITPROJECTS} || mkdir ${GITPROJECTS}
 
 # define function used later in processing
 function loglogs() {
-    echo ..Logfiles after ${phase} phase
-    ls ${BUILD_PREFIX}/*.err > /dev/null 2>&1 && ls ${BUILD_PREFIX}/*.err
-#   ls ${BUILD_PREFIX}/*.ok  > /dev/null 2>&1 && ls ${BUILD_PREFIX}/*.ok
+    echo "..Logfiles after ${phase} phase"
+    ls ${BUILD_PREFIX}/*.err > /dev/null 2>&1 && ls ${BUILD_PREFIX}/*.err || echo '..None for *.err'
+#   ls ${BUILD_PREFIX}/*.ok  > /dev/null 2>&1 && ls ${BUILD_PREFIX}/*.ok  || echo '..None for *.ok'
     #read -p "Press ENTER to continue: "
     return 0
 }
@@ -104,29 +105,31 @@ function loglogs() {
 # if errors from previous run, remove them before restarting
 for project in gsl libsodium libzmq czmq malamute zyre; do
   for phase in gsl-generation building autogen-config make make-install make-check; do
-    test -f ${BUILD_PREFIX}/${project}_${phase}.err && rm -f ${BUILD_PREFIX}/${project}_${phase}.err 
+    test -f ${BUILD_PREFIX}/${project}_${phase}.err && rm -f ${BUILD_PREFIX}/${project}_${phase}.err
     test -f ${BUILD_PREFIX}/${project}_${phase}.ok  && rm -f ${BUILD_PREFIX}/${project}_${phase}.ok
   done
 done
+phase="initial cleanup"
 loglogs
 
 # build zproject with any changes made to it
-echo Building zproject
+echo "Building zproject"
 phase=building
 (
-    cd ../zproject &&
-    ./autogen.sh &&
-    ./configure &&
-    make &&
-    make install &&
+    cd ../zproject && \
+    ./autogen.sh && \
+    ./configure && \
+    make && \
+    make install
     exit $?
-) > ${BUILD_PREFIX}/zproject_${phase}.err 2>&1 && mv ${BUILD_PREFIX}/zproject_${phase}.err ${BUILD_PREFIX}/zproject_${phase}.ok
+) > ${BUILD_PREFIX}/zproject_${phase}.err 2>&1 && \
+    mv -f ${BUILD_PREFIX}/zproject_${phase}.err ${BUILD_PREFIX}/zproject_${phase}.ok
 loglogs
 
 
 # go where we will clone our target projects
 pushd ${GITPROJECTS} > /dev/null 2>&1
-echo Projects will be cloned to here "${PWD}"
+echo "Projects will be cloned to here: ${PWD}"
 #read -p "Press ENTER to continue: "
 
 # get required but not generated projects for zeromq stack
@@ -139,71 +142,74 @@ test -d malamute  || git clone --depth 1 https://github.com/${ZEROMQ}/malamute  
 test -d zyre      || git clone --depth 1 https://github.com/${ZEROMQ}/zyre            zyre
 
 # build gsl (the generator)
-echo Building gsl
+echo "Building gsl"
 phase=building
 (
-    cd ${GITPROJECTS}/gsl/src &&
-    make -j4 &&
-    DESTDIR=${BUILD_PREFIX} make install &&
+    cd ${GITPROJECTS}/gsl/src && \
+    make -j4 && \
+    DESTDIR=${BUILD_PREFIX} make install
     exit $?
-) > ${BUILD_PREFIX}/gsl_${phase}.err 2>&1 &&
-    mv ${BUILD_PREFIX}/gsl_${phase}.err ${BUILD_PREFIX}/gsl_${phase}.ok
+) > ${BUILD_PREFIX}/gsl_${phase}.err 2>&1 && \
+    mv -f ${BUILD_PREFIX}/gsl_${phase}.err ${BUILD_PREFIX}/gsl_${phase}.ok
 loglogs
 
 
 #regenerate projects
-echo Regenerating projects
 phase=gsl-generation
 for project in czmq malamute zyre; do
     (
-        cd ${GITPROJECTS}/$project &&
-        ${BUILD_PREFIX}/bin/gsl project.xml &&
+        echo "Regenerating projects: $project"
+        cd ${GITPROJECTS}/$project && \
+        ${BUILD_PREFIX}/bin/gsl project.xml
         exit $?
-    ) > ${BUILD_PREFIX}/${project}_${phase}.err 2>&1 && test 0 -eq $? &&
-        mv ${BUILD_PREFIX}/${project}_${phase}.err  ${BUILD_PREFIX}/${project}_${phase}.ok
+    ) > ${BUILD_PREFIX}/${project}_${phase}.err 2>&1 && \
+        mv -f ${BUILD_PREFIX}/${project}_${phase}.err ${BUILD_PREFIX}/${project}_${phase}.ok
 done
 loglogs
 
 
-echo "All projects have been regenerated. Good time for a bacup if you want."
+echo "All projects have been regenerated. Good time for a backup if you want."
 read -p "Press ENTER to continue: "
 
 
 # build zeromq stack including regenerated projects
 
 # testing building
-echo Building zeromq stack components
+echo "Building zeromq stack components"
 phase=autogen-config
 for project in libsodium libzmq czmq malamute zyre; do
     (
-        cd ${GITPROJECTS}/$project &&
-        ./autogen.sh &&
-        ./configure --prefix=${BUILD_PREFIX} &&
+        echo "Configuring projects: $project"
+        cd ${GITPROJECTS}/$project && \
+        ./autogen.sh && \
+        ./configure --prefix=${BUILD_PREFIX}
         exit $?
-    ) > ${BUILD_PREFIX}/${project}_${phase}.err 2>&1 &&
-        mv ${BUILD_PREFIX}/${project}_${phase}.err  ${BUILD_PREFIX}/${project}_${phase}.ok
+    ) > ${BUILD_PREFIX}/${project}_${phase}.err 2>&1 && \
+        mv -f ${BUILD_PREFIX}/${project}_${phase}.err ${BUILD_PREFIX}/${project}_${phase}.ok
 done
 loglogs
 
 phase=make
 for project in libsodium libzmq czmq malamute zyre; do
     (
-        cd ${GITPROJECTS}/$project &&
-        make &&
+        echo "Building projects: $project"
+        cd ${GITPROJECTS}/$project && \
+        make
         exit $?
-    ) > ${BUILD_PREFIX}/${project}_${phase}.err 2>&1 &&
-        mv ${BUILD_PREFIX}/${project}_${phase}.err  ${BUILD_PREFIX}/${project}_${phase}.ok
+    ) > ${BUILD_PREFIX}/${project}_${phase}.err 2>&1 && \
+        mv -f ${BUILD_PREFIX}/${project}_${phase}.err ${BUILD_PREFIX}/${project}_${phase}.ok
 done
 loglogs
 
 phase=make-install
 for project in libsodium libzmq czmq malamute zyre; do
     (
-        cd ${GITPROJECTS}/$project &&
-        DESTDIR=${BUILD_PREFIX} make install &&
+        echo "Installing projects: $project"
+        cd ${GITPROJECTS}/$project && \
+        DESTDIR=${BUILD_PREFIX} make install
         exit $?
-    ) > ${BUILD_PREFIX}/${project}_${phase}.err 2>&1 &&
-        mv ${BUILD_PREFIX}/${project}_${phase}.err  ${BUILD_PREFIX}/${project}_${phase}.ok
+    ) > ${BUILD_PREFIX}/${project}_${phase}.err 2>&1 && \
+        mv -f ${BUILD_PREFIX}/${project}_${phase}.err ${BUILD_PREFIX}/${project}_${phase}.ok
 done
 loglogs
 
@@ -212,18 +218,19 @@ echo Running tests
 phase=make-check
 for project in libzmq czmq malamute zyre; do
     (
-        cd ${GITPROJECTS}/$project &&
-        DESTDIR=${BUILD_PREFIX} PATH=${BUILD_PREFIX}/bin:$PATH make check &&
+        echo "Checking projects: $project"
+        cd ${GITPROJECTS}/$project && \
+        DESTDIR=${BUILD_PREFIX} PATH=${BUILD_PREFIX}/bin:$PATH make check
         exit $?
-    ) > ${BUILD_PREFIX}/${project}_${phase}.err 2>&1 &&
-        mv ${BUILD_PREFIX}/${project}_${phase}.err  ${BUILD_PREFIX}/${project}_${phase}.ok
+    ) > ${BUILD_PREFIX}/${project}_${phase}.err 2>&1 && \
+        mv -f ${BUILD_PREFIX}/${project}_${phase}.err ${BUILD_PREFIX}/${project}_${phase}.ok
 done
 loglogs
 
 # look for the word "FAIL" in .err files and report them
 function logfailed() {
-    echo Problems during $1:
-    grep -iw -e fail $1 
+    echo "Problems during $1:"
+    grep -iw -e fail "$1"
     return 1
 }
 
@@ -231,15 +238,15 @@ function logfailed() {
 finalresult=0
 for project in zproject gsl libsodium libzmq czmq malamute zyre; do
   for phase in gsl-generation building autogen-config make make-install make-check; do
-    test -f ${BUILD_PREFIX}/${project}_${phase}.err &&
+    test -f ${BUILD_PREFIX}/${project}_${phase}.err && \
+            finalresult=1 && \
             logfailed ${BUILD_PREFIX}/${project}_${phase}.err
   done
 done
-finalresult=$?
 
 popd > /dev/null 2>&1
 
-echo STOP__DATE: `date`
-echo START_DATE: $STARTDATE
+echo "STOP__DATE: `date`"
+echo "START_DATE: $STARTDATE"
 
 exit ${finalresult}
