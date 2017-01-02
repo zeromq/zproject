28f05c4463c25cdfe16d0595450e94472cb2eb90
Jim Klimov <jim@jimklimov.com>
2017-01-02 15:09:17 +0100

    ci_build.sh : add basic ccache support (for GSL mostly)

--- ci_build.sh
+++ ci_build.sh
@@ -4,6 +4,7 @@
 # This script is used by travis ci to test updates for zproject itself:
 # it builds a latest GSL, then zproject, and tests it by regenerating
 # a stable consumer project (CZMQ) which is expected to pass well.
+# Optionally speeds up the compilation steps using ccache (stashed).
 #
 
 set -x
@@ -13,10 +14,17 @@ if [ "$BUILD_TYPE" == "default" ]; then
     mkdir tmp
     BUILD_PREFIX="$PWD/tmp"
 
+    CCACHE_PATH="$PATH"
+    CCACHE_DIR="${HOME}/.ccache"
+    export CCACHE_PATH CCACHE_DIR
+    # ccache -s 2>/dev/null || true
+
     if ! ((command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list generator-scripting-language >/dev/null 2>&1) || \
            (command -v brew >/dev/null 2>&1 && brew ls --versions gsl >/dev/null 2>&1)); then
         git clone --depth 1 https://github.com/imatix/gsl.git gsl
         ( cd gsl/src && \
+          CCACHE_BASEDIR=${PWD} && \
+          export CCACHE_BASEDIR && \
           make -j4 && \
           DESTDIR="${BUILD_PREFIX}" make install \
         ) || exit 1
@@ -24,6 +32,8 @@ if [ "$BUILD_TYPE" == "default" ]; then
 
     ( ./autogen.sh && \
       PATH="${BUILD_PREFIX}/bin:$PATH" && export PATH && \
+      CCACHE_BASEDIR=${PWD} && \
+      export CCACHE_BASEDIR && \
       ./configure --prefix="${BUILD_PREFIX}" && \
       make && \
       make install \
@@ -34,8 +44,14 @@ if [ "$BUILD_TYPE" == "default" ]; then
     git clone --depth 1 https://github.com/zeromq/czmq.git czmq
     ( PATH="${BUILD_PREFIX}/bin:$PATH"; export PATH; \
       cd czmq && \
+      CCACHE_BASEDIR=${PWD} && \
+      export CCACHE_BASEDIR && \
       gsl -target:* project.xml \
     ) || exit 1
+
+    echo "=== How well did ccache help on this platform?"
+    ccache -s 2>/dev/null || true
+    echo "==="
 else
     pushd "./builds/${BUILD_TYPE}" && \
     REPO_DIR="$(dirs -l +1)" ./ci_build.sh \
